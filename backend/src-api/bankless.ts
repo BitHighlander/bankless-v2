c978680d1b736266be341264473089e171c12a18
bithighlander <bithighlander@gmail.com>
2023-12-20 19:39:57 -0700

    count after clear

--- backend/src-api/bankless.ts
+++ backend/src-api/bankless.ts
@@ -770,6 +770,7 @@ module.exports = {
 let clear_session = function () {
     let tag = TAG + " | clear_session | "
     try {
+        
         CURRENT_SESSION = {
             sessionId: null,
             type: null,
@@ -785,6 +786,7 @@ let clear_session = function () {
         if(!ATM_NO_HARDWARE){
             eSSP.disable()
         }
+        countBills()
     } catch (e) {
         console.error(tag, "e: ", e)
         throw e
